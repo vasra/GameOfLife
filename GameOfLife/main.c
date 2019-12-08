62be6e4703d1022af5fc4db09ac1954113f6562c
vasra <41552373+vasra@users.noreply.github.com>
2019-12-08 23:16:14 +0200

    Almost finished. Must implement an infinite loop for Life to run indefinitely.

--- GameOfLife/main.c
+++ GameOfLife/main.c
@@ -1,30 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define SIZE 16
+#define SIZE 4
 
-void dead_state(int width, int height, char life_grid[SIZE][SIZE]);
-void print_grid(char life_grid[SIZE][SIZE]);
+void initial_state(char life_beginning[SIZE][SIZE]);
+void print_grid(char life[SIZE][SIZE]);
+void next_generation(char life_current_generation[SIZE][SIZE], char life_next_generation[SIZE][SIZE]);
 
 int main()
 {
-    char life_grid[SIZE][SIZE];
+    char life_beginning[SIZE][SIZE];
+    char life_next_generation[SIZE][SIZE];
+    srand(time(NULL));
+
+    initial_state(life_beginning);
+    print_grid(life_beginning);
+    next_generation(life_beginning, life_next_generation);
+    print_grid(life_next_generation);
+    return 0;
+}
+
+void initial_state(char life_beginning[SIZE][SIZE])
+{
+    float probability;
 
     for(int i = 0; i < SIZE; i++)
     {
         for(int j = 0; j < SIZE; j++)
-            life_grid[i][j] = 'X';
+        {
+            probability = (float)rand() / (float)((unsigned)RAND_MAX + 1);
+            if(probability >= 0.5f)
+                life_beginning[i][j] = 'X';
+            else
+                life_beginning[i][j] = '-';
+        }
     }
-    print_grid(life_grid);
-    return 0;
 }
 
-void dead_state(int width, int height, char life_grid[SIZE][SIZE])
-{
-    life_grid[width][height] = '-';
-}
-
-void print_grid(char life_grid[SIZE][SIZE])
+void print_grid(char life[SIZE][SIZE])
 {
     printf("------------------------------------\n");
     printf("    Welcome to the Game Of Life!\n");
@@ -34,9 +48,42 @@ void print_grid(char life_grid[SIZE][SIZE])
     {
         for(int j = 0; j < SIZE; j++)
         {
-            printf("%c ", life_grid[i][j]);
+            printf("%c ", life[i][j]);
             if( j == SIZE - 1)
                 printf("\n");
         }
+    }
+}
+
+void next_generation(char life_current_generation[SIZE][SIZE], char life_next_generation[SIZE][SIZE])
+{
+    int neighbors;
+
+    for(int i = 0; i < SIZE; i++)
+    {
+        for(int j = 0; j < SIZE; j++)
+        {
+            neighbors = 0;
+            for(int k = i - 1; k <= i + 1; k++)
+            {
+                for(int l = j - 1; l <= j + 1; l++)
+                {
+                    if(k > -1 && k < SIZE && l > -1 && l < SIZE)
+                    {
+                        if(life_current_generation[k][l] == 'X')
+                            neighbors++;
+                    }
+                }
+            }
+            if(life_current_generation[i][j] == 'X')
+                neighbors--;
+
+                printf("Neighbors %d ", neighbors);
+            if(neighbors == 3 || (neighbors == 2 && life_current_generation[i][j] == 'X'))
+                life_next_generation[i][j] = 'X';
+            else
+                life_next_generation[i][j] = '-';
+        }
+        printf("\n");
     }
 }
