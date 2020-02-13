cb5248bcaa0191192aa14a530fe2ebb8020a4e5d
vasra <41552373+vasra@users.noreply.github.com>
2020-02-13 18:34:22 +0200

    Ready for some (very basic) testing of the Cartesian topology.

--- GameOfLife/main.c
+++ GameOfLife/main.c
@@ -14,34 +14,47 @@ void swap(char (**a)[SIZE], char (**b)[SIZE]);
 
 int main()
 {
+    /**< Our 2D grid in memory, and its necessary copy. */
     char life[SIZE][SIZE];
     char life_copy[SIZE][SIZE];
+
+    /**< Pointers to the above grids. */
     char (*current_generation)[SIZE] = life;
     char (*current_generation_copy)[SIZE] = life_copy;
 
-    int dim_size[NDIMS], periods[NDIMS], reorder, rank, nrow, ncol;
+    /*******************************************************************************
+     * ARRAYS FOR THE CARTESIAN TOPOLOGY
+     * dim_size - Array with two elements
+     *     dim_size[0] - How many processes will be in each row
+     *     dim_size[1] - How many processes will be in each column
+     *
+     * periods  - Array with two elements, for the periodicity of the two dimensions
+     *
+     * coords   - Array with two elements, holding the coordinates of each process
+     *******************************************************************************/
+
+    int dim_size[NDIMS], periods[NDIMS], coords[NDIMS];
+
+    /*******************************************************************************************************
+     * VARIABLES FOR THE CARTESIAN TOPOLOGY
+     * reorder         - Indicates if MPI can rearrange the processes more efficiently among the processors
+     * rank            - Process rank
+     * my_row_index    - Where each process starts in the rows of the 2D matrix
+     * my_column_index - Where each process starts in the columns of the 2D matrix
+     * cartesian2D     - Our new custom Communicator
+     *******************************************************************************************************/
+
+    int reorder, rank, my_row_index, my_column_index;
     MPI_Comm cartesian2D;
 
-    /*
-     *
-     * MISTAKE! FIX!
-     */
-    if( PROCESSES * PROCESSES == SIZE )
-        dims[0] = dims[1] = PROCESSES;
-    else
-    {
-        dims[0] = SIZE / PROCESSES;
-        dims[1] = dims[0] / 2;
-    }
-
-    /* Our Cartesian topology will be a torus, so both fields of "periods" will have value of 1 */
+    /**< Our Cartesian topology will be a torus, so both fields of "periods" array will have a value of 1. */
     periods[0] = 1;
     periods[1] = 1;
 
-    /* We allow MPI to efficiently reorder the processes among the different processors. */
+    /**< We will allow MPI to efficiently reorder the processes among the different processors. */
     reorder = 1;
 
-    /* Seed the random number generator, and generate the first generation according to that. */
+     /**< Seed the random number generator, and generate the first generation according to that. */
     initial_state(current_generation, current_generation_copy);
 
     printf("------------------------------------\n");
@@ -50,31 +63,68 @@ int main()
 
     print_grid(current_generation);
 
+    /**< Initialize MPI */
     MPI_Init(NULL, NULL);
-    MPI_Cart_create(MPI_COMM_WORLD, NDIMS, dims, periods, reorder, &cartesian2D);
-    MPI_Comm_size(cartesian2D, &processes);
-    /*
-     * The Game Of Life will run for 5 generations. Modify the number
-     * of generations as desired.
-     */
-    for(int i = 0; i < 4; i++)
-    {
-        next_generation(current_generation, current_generation_copy);
-        print_grid(current_generation);
 
-        /*
+    /**< First, find the rank of each process in the default communicator. */
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    /**< Let MPI decide which is the best arrangement according to the number of processes and dimensions. */
+    MPI_Dims_create(PROCESSES, NDIMS, dim_size);
+
+    /**< Create a 2D Cartesian topology. */
+    MPI_Cart_create(MPI_COMM_WORLD, NDIMS, dim_size, periods, reorder, &cartesian2D);
+
+    /**< Synchronize all the processes before we start. */
+    MPI_Barrier(cartesian2D);
+
+    /**< We must find the rank of each process. This must run only once, process 0 can handle it. */
+    if(rank == 0)
+    {
+        printf("There are %d dimensions and %d processes\n", NDIMS, PROCESSES);
+        for(i = 0; i < dim_size[0]; i++)
+        {
+            for(int j = 0; j < dim_size[1]; j++)
+            {
+                printf("My rank is %d\n", rank);
+                coords[0] = i;
+                coords[1] = j;
+                MPI_Cart_rank(cartesian2D, coords, &rank);
+                printf("The rank for coords[%d][%d] is %d\n", i, j, rank);
+            }
+        }
+    }
+
+    my_row_index    = rank * (SIZE / dim_size[0]);
+    my_column_index = rank * (SIZE / dim_size[1]);
+    printf("Process %d: My row index is %d and my column index is %d\n", rank, my_row_index, my_column_index);
+
+    /***********************************************
+     * The Game Of Life will run for 5 generations.
+     * Modify the number of generations as desired.
+     ***********************************************/
+//    for(int i = 0; i < 4; i++)
+//    {
+//        next_generation(current_generation, current_generation_copy);
+//        print_grid(current_generation);
+
+        /************************************************************************************************
          * Swap the addresses of the two tables. That way, we avoid copying the contents
          * of current_generation to current_generation_copy in the next_generation function.
          * Each round, the addresses are exchanged, saving time from running a loop to copy the contents.
-         */
-        swap(&current_generation, &current_generation_copy);
-    }
+         ************************************************************************************************/
+//        swap(&current_generation, &current_generation_copy);
+//    }
+
+    /**< Clean up and exit. */
+    MPI_Finalize();
     return 0;
 }
 
-/* Randomly generates the first generation. The living organisms
- * are represented by an 1, and the dead organisms by a 0.
- */
+/****************************************************************
+ * Randomly generates the first generation. The living organisms
+ * are represented by a 1, and the dead organisms by a 0.
+ ****************************************************************/
 void initial_state(char first_generation[SIZE][SIZE], char first_generation_copy[SIZE][SIZE])
 {
     float probability;
@@ -93,6 +143,9 @@ void initial_state(char first_generation[SIZE][SIZE], char first_generation_copy
     }
 }
 
+/****************************************************************
+ * Prints the entire grid to the terminal.
+ ****************************************************************/
 void print_grid(char life[SIZE][SIZE])
 {
     for (int i = 0; i< SIZE; i++)
@@ -107,15 +160,14 @@ void print_grid(char life[SIZE][SIZE])
     printf("\n");
 }
 
-/*
+/*************************************************************************************
  * Produces the next generation. It checks the contents of current_generation_copy,
  * calculates the results, and stores them in current_generation. The living organisms
  * are represented by a 1, and the dead organisms by a 0.
- */
+ *************************************************************************************/
 void inline next_generation(char current_generation[SIZE][SIZE], char current_generation_copy[SIZE][SIZE])
 {
     int neighbors;
-
     for(int i = 0; i < SIZE; i++)
     {
         for(int j = 0; j < SIZE; j++)
@@ -141,19 +193,6 @@ void inline next_generation(char current_generation[SIZE][SIZE], char current_ge
                 current_generation[i][j] = 0;
         }
     }
-
-    /*
-     * This nested loop is not needed anymore. Instead of copying the contents of current_generation
-     * into current_generation_copy every round, we simply swap their addresses
-
-    for(int i = 0; i < SIZE; i++)
-    {
-        for(int j = 0; j < SIZE; j++)
-        {
-            current_generation_copy[i][j] = current_generation[i][j];
-        }
-    }
-     */
 }
 
 void inline swap(char (**a)[SIZE], char (**b)[SIZE])
