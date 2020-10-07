9073c30d0ac81f4a1691ea0fffd92fa4fcafe669
vasra <41552373+vasra@users.noreply.github.com>
2020-10-07 23:51:41 +0300

    Fixed the if of Allreduce.

--- GameOfLife/life.c
+++ GameOfLife/life.c
@@ -9,9 +9,9 @@
 #define SIZE 840
 #define NDIMS 2
 #define BLOCKS
-#define DEBUG_COORDINATES
-#define DEBUG_GRID
-#define ALL_REDUCE
+//#define DEBUG_COORDINATES
+//#define DEBUG_GRID
+//#define ALL_REDUCE
 
 void Initial_state(int rows, int columns, char *first_generation, char *first_generation_copy, int seed, int *local_sum, int *global_sum, MPI_Comm *cartesian2D, int rank);
 void Print_grid(int rows, int columns, char *life);
@@ -307,7 +307,7 @@ int main()
 #endif
 
     /* Modify the number of generations as desired */
-    for (int generation = 0; generation < 5; generation++)
+    for (int generation = 0; generation < 2000; generation++)
     {
         if (generation % 2 == 0)
         {
@@ -372,7 +372,7 @@ int main()
             * a loop to copy the contents.
             ************************************************************************************************/
             Swap(&life, &life_copy);
-            if (generation >= 10 && generation % 10 == 0)
+            if (generation % 10 == 0)
                 MPI_Allreduce(&local_sum, &global_sum, 1, MPI_INT, MPI_SUM, cartesian2D);
 
             local_sum = 0;
@@ -441,7 +441,7 @@ int main()
             * a loop to copy the contents.
             ************************************************************************************************/
             Swap(&life, &life_copy);
-            if (generation >= 10 && generation % 10 == 0)
+            if (generation % 10 == 0)
                 MPI_Allreduce(&local_sum, &global_sum, 1, MPI_INT, MPI_SUM, cartesian2D);
 
             local_sum = 0;
